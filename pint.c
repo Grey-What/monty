380d359828323595a4e05203cd49f75e16e52bf2
Vish <vishaylinparshotam@gmail.com>
2024-01-18 03:43:32 -0800

    indentation fix

--- pint.c
+++ pint.c
@@ -2,7 +2,7 @@
 /**
 * pint - prints the value on top of the stack.
 * @stack: pointer to the head of a linked list
-* @line_number: line number 
+* @line_number: line number
 * Return: void
 */
 void pint(stack_t **stack, unsigned int line_number)
@@ -10,7 +10,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty/n", line_number);
-		exit(EXIT_FAILURE); 
+		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
 }
