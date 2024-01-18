610042e0d0918bf2b110f65a601cf5ea26e7d95c
Grey-What <greywhat2002@gmail.com>
2024-01-18 04:53:02 -0800

    updated

--- freestack.c
+++ freestack.c
@@ -2,19 +2,19 @@
 /**
  * free_stack - frees adoubly linked list stack in reverse with top pointer
  *
- * @head: pointer to head of linked list/bottom of stack
+ * @stack: pointer to top of stack
  *
  * Return: void
  */
-void free_stack(stack_t **head)
+void free_stack(stack_t **stack)
 {
 	stack_t *temp = NULL;
 
-	while ((*head) != NULL)
+	while ((*stack) != NULL)
 	{
-		temp = (*head);
-		(*head) = (*head)->next;
+		temp = (*stack);
+		(*stack) = (*stack)->prev;
 		free(temp);
 	}
-	free(*head);
+	free(*stack);
 }
