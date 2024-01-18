ae4bc42699a9ba13028e90fdf4fe5e5da5bc6716
Vish <vishaylinparshotam@gmail.com>
2024-01-18 06:39:45 -0800

    error fix

--- add.c
+++ add.c
@@ -8,7 +8,7 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	int res = 0;
 
-	if ((*stack) == NULL || (*stack-)>prev == NULL)
+	if ((*stack) == NULL || (*stack)->prev == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short", line_number);
 		exit(EXIT_FAILURE);
