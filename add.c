d9363eaae4a8cceb688609d4e8100746caf8ae30
Vish <vishaylinparshotam@gmail.com>
2024-01-18 06:27:34 -0800

    create add function

---
+++ add.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+* add - adds the top two elements of the stack
+* @stack: points to the stack
+* Return: void
+*/
+void add(stack_t **stack, unsigned int line_number)
+{
+	int res = 0;
+
+	if ((*stack) == NULL || (*stack-)>prev == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short", line_number);
+		exit(EXIT_FAILURE);
+	}
+	res = (*stack)->n + ((*stack)->prev)->n;
+	pop(stack, line_number);
+	(*stack)->n = res;
+}
